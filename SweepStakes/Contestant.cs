26f210090beefc451f5c21ecd206500502c0b98d
jrapkin <jrap1243@gmail.com>
2020-03-02 11:50:56 -0600

    Added the following classes: UserInterface, Contestant, Sweepstakes, SweepstakesStackManager, SweepstakesQueueManager, MarketingFirm. These will be the core classes used in the Sweepstakes solution. I also added a ISweepstakesManager interface from which both SweepstakesStackManager and SweepstakesQueueManager will be contracted to.

---
+++ SweepStakes/Contestant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+	class Contestant
+	{
+		//member variables
+		private string firstName;
+		private string lastName;
+		private string emailAddress;
+		private int registrationNumber;
+
+		//ctor
+		public Contestant()
+		{
+
+		}
+
+		//member methods
+	}
+}
